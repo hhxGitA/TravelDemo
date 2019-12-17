498f1030b3130175048307815a7471eee2f42807
hhxGitA <1678267728@qq.com>
2019-12-17 10:41:08 +0800

    更新7.2

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Home from '../pages/home/Home'
+import Yellow from '../pages/Yellow'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/yellow',
+      name: 'Yellow',
+      component: Yellow
     }
   ]
 })
