bdbeb3c6f5e4221888986d51f56c3118c57aef1a
hhxGitA <1678267728@qq.com>
2019-12-17 14:23:21 +0800

    '7.3'

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img"
+             :src="item.imgUrl">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: '//imgs.qunarzz.com/sight/p0/1411/34/6aec007588037c2d9ef339d81aeba256.water.jpg_256x160_ec997312.jpg'
+      }, {
+        id: '0002',
+        imgUrl: '//imgs.qunarzz.com/vc/8d/63/a8/90bc4af557172cc68c6725ec36.jpg_92.jpg'
+      }]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background :white
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 40%
+    .swiper-img
+      width 100%
+      height 3rem
+</style>
